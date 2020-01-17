4ef5f8eca1b54d90516040efa26df892b2cf5590
archification <archification@gmail.com>
2020-01-17 08:42:09 -0600

    first commit

---
+++ src/vectors.rs
@@ -0,0 +1,34 @@
+// Vectors are resizable arrays
+
+use std::mem;
+
+pub fn run() {
+    let mut numbers: Vec<i32> = vec![1, 2, 3, 4];
+
+    numbers[2] = 20;
+
+    // Add to vector
+    numbers.push(5);
+    numbers.push(6);
+
+    // Pop off last value
+    numbers.pop();
+
+    println!("{:?}", numbers);
+    println!("Single Value: {}", numbers[0]);
+    println!("Vector Length: {}", numbers.len());
+    println!("Vector occupies {} bytes", mem::size_of_val(&numbers));
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice);
+
+    // Loop through vector values
+    for x in numbers.iter() {
+        println!("Number: {}", x);
+    }
+
+    // Loop and mutate values
+    for x in numbers.iter_mut() {
+        *x *= 2;
+    }
+    println!("Numbers Vec: {:?}", numbers);
+}
